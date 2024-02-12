f35cb290e37b408e000b4b870973aa0e1a354ef4
Benny Halperin <bhalperin@gmail.com>
2024-02-12 08:33:59 +0200

    Created About component

--- apps/ui/src/app/app.routes.ts
+++ apps/ui/src/app/app.routes.ts
@@ -2,7 +2,17 @@ import { Route } from '@angular/router';
 
 export const appRoutes: Route[] = [
 	{
+		path: 'about',
+		loadComponent: () =>
+			import('./components/about/about.component').then(
+				(c) => c.AboutComponent,
+			),
+	},
+	{
 		path: 'users',
-		loadComponent: () => import('./components/users/users.component').then(c => c.UsersComponent)
-	}
+		loadComponent: () =>
+			import('./components/users/users.component').then(
+				(c) => c.UsersComponent,
+			),
+	},
 ];
