4b36c80ef871d552e5d2e782ee8f71d7d2dfdcbd
Benny Halperin <bhalperin@gmail.com>
2024-02-12 08:34:18 +0200

    Created full user mock

---
+++ libs/shared/src/lib/models/mocks/fulluser.mock.ts
@@ -0,0 +1,43 @@
+import { GhFullUser } from '../user.model';
+import { ModelMock } from './model.mock';
+
+export class GhFullUserMock extends ModelMock<GhFullUser> {
+	constructor() {
+		super();
+		this.data = {
+			avatar_url: '',
+			html_url: 'http://www.google.com',
+			id: 1,
+			login: '',
+			bio: '',
+			blog: '',
+			location: '',
+			name: '',
+			public_repos: 0
+		};
+	}
+
+	withAvatarUrl(avatarUrl: string): GhFullUserMock {
+		this.data.avatar_url = avatarUrl;
+
+		return this;
+	}
+
+	withHtmlUrl(htmlUrl: string): GhFullUserMock {
+		this.data.html_url = htmlUrl;
+
+		return this;
+	}
+
+	withId(id: number): GhFullUserMock {
+		this.data.id = id;
+
+		return this;
+	}
+
+	withLogin(login: string): GhFullUserMock {
+		this.data.login = login;
+
+		return this;
+	}
+}
