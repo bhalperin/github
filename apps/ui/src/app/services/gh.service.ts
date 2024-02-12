19c3e5b4dd9a212e4336e21087fd7934058f3928
Benny Halperin <bhalperin@gmail.com>
2024-02-12 08:40:30 +0200

    Added simple catch error to github api call

--- apps/ui/src/app/services/gh.service.ts
+++ apps/ui/src/app/services/gh.service.ts
@@ -1,21 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { GhFullUser, GhUser } from '@gh/shared';
-import { Observable } from 'rxjs';
+import { Observable, catchError } from 'rxjs';
 
 @Injectable({
-	providedIn: 'root'
+	providedIn: 'root',
 })
 export class GhService {
 	readonly #http = inject(HttpClient);
 	readonly #baseApiUrl = '/api';
 
-	constructor() { }
-
 	getUsers(): Observable<GhUser[]> {
 		const url = `${this.#baseApiUrl}/users`;
 
-		return this.#http.get<GhUser[]>(url);
+		return this.#http.get<GhUser[]>(url).pipe(
+			catchError((error) => {
+				console.error('http error:', error);
+				return [];
+			}),
+		);
 	}
 
 	getUser(login: string): Observable<GhFullUser> {
