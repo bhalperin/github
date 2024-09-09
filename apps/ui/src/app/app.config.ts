69d1b17a21f632e4d98f654d31cfd352b3501e8e
Benny Halperin <bhalperin@gmail.com>
2024-09-09 11:20:08 +0300

    Improved navigation bar and loader styles

--- apps/ui/src/app/app.config.ts
+++ apps/ui/src/app/app.config.ts
@@ -1,13 +1,13 @@
-import { AppRouter } from './fw-extensions/app-router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideExperimentalZonelessChangeDetection } from '@angular/core';
-import { provideRouter, Router } from '@angular/router';
+import { provideRouter, Router, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { AppRouter } from './fw-extensions/app-router';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
-		provideRouter(appRoutes),
+		provideRouter(appRoutes, withComponentInputBinding()),
 		provideHttpClient(withInterceptors([authInterceptor])),
 		provideExperimentalZonelessChangeDetection(),
 		{
