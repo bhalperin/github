d6368c273151f59530ff3d32a292098b2f62a5ce
Benny Halperin <bhalperin@gmail.com>
2024-09-18 06:00:53 +0300

    Formatted test file

--- apps/ui/src/app/services/gh.service.spec.ts
+++ apps/ui/src/app/services/gh.service.spec.ts
@@ -1,16 +1,15 @@
 import { TestBed } from '@angular/core/testing';
-
 import { GhService } from './gh.service';
 
 describe('GhService', () => {
-  let service: GhService;
+	let service: GhService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(GhService);
-  });
+	beforeEach(() => {
+		TestBed.configureTestingModule({});
+		service = TestBed.inject(GhService);
+	});
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+	it('should be created', () => {
+		expect(service).toBeTruthy();
+	});
 });
