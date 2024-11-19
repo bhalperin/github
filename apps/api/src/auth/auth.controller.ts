8a0142b4874cc82b78f7b45c5f1d1ff12cef2781
Benny Halperin <bhalperin@gmail.com>
2024-11-19 11:36:24 +0200

    Integrated Swagger (initial work)

--- apps/api/src/auth/auth.controller.ts
+++ apps/api/src/auth/auth.controller.ts
@@ -1,13 +1,16 @@
 import { AuthKeys } from '@gh/shared';
 import { Body, Controller, Get, HttpCode, HttpStatus, Inject, Post, Req, Res, UnauthorizedException, UseGuards } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { ApiBody } from '@nestjs/swagger';
+import { User as PrismaUser } from '@prisma/client';
+import { Request, Response } from 'express';
 import { globalConfig } from '../config';
 import { messageWhenCalled } from '../utils/decorators';
 import { AuthService } from './auth.service';
+import { LoginDto } from './dtos';
 import { GoogleAuthGuard } from './google-auth.guard';
 import { JwtAuthGuard } from './jwt-auth.guard';
 import { LocalAuthGuard } from './local-auth.guard';
-import { Request, Response } from 'express';
 
 @Controller('auth')
 export class AuthController {
@@ -19,8 +22,10 @@ export class AuthController {
 	@UseGuards(LocalAuthGuard)
 	@Post('login')
 	@HttpCode(HttpStatus.OK)
-	async login(@Req() req, @Res() res: Response) {
+	@ApiBody({ type: LoginDto })
 	@messageWhenCalled('Log in with user/password')
+	async login(@Body() user: LoginDto, @Res() res: Response) {
+		const response = await this.authService.login(user as PrismaUser);
 
 		res.cookie(AuthKeys.AccessToken, response.accessToken, { secure: true });
 		res.cookie(AuthKeys.RefreshToken, response.refreshToken, { secure: true });
