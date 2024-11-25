9118dc2b574304ea272f3bfdbe4bb44f8058a67f
Benny Halperin <bhalperin@gmail.com>
2024-11-25 08:16:23 +0200

    Linting and code formatting

--- apps/ui/src/app/store/app.store.ts
+++ apps/ui/src/app/store/app.store.ts
@@ -1,10 +1,5 @@
-import {
-	patchState,
-	signalStore,
-	withHooks,
-	withMethods,
-	withState,
-} from '@ngrx/signals';
+import { computed } from '@angular/core';
+import { patchState, signalStore, withComputed, withHooks, withMethods, withState } from '@ngrx/signals';
 
 export type UserCardState = {
 	id: number;
@@ -36,36 +31,32 @@ export const initialState = {
 export const AppStore = signalStore(
 	{ providedIn: 'root' },
 	withState(initialState),
-	withMethods((state) => {
-		return {
-			isUserAuthenticated: () => state.user.authenticated(),
-			authenticateUser: (authenticated: boolean) => patchState(state, { user: { authenticated }}),
-			resetUserCards: () => {
-				patchState(state, { gh: { users: { cards: [] } } });
-			},
-			isUserCardFlipped: (id: number) => {
-				return (
-					state.gh.users.cards().find((card) => card.id === id)?.flipped ?? false
-				);
-			},
-			updateUserCards: (id: number, flipped: boolean) => {
-				const cards = [...state.gh.users.cards()];
-				const userCardState = cards.find((card) => card.id === id);
+	withComputed((state) => ({
+		isUserAuthenticated: computed(() => state.user().authenticated),
+	})),
+	withMethods((state) => ({
+		authenticateUser: (authenticated: boolean) => patchState(state, { user: { authenticated } }),
+		resetUserCards: () => {
+			patchState(state, { gh: { users: { cards: [] } } });
+		},
+		isUserCardFlipped: (id: number) => state.gh.users.cards().find((card) => card.id === id)?.flipped ?? false,
+		updateUserCards: (id: number, flipped: boolean) => {
+			const cards = [...state.gh.users.cards()];
+			const userCardState = cards.find((card) => card.id === id);
 
-				if (flipped) {
-					if (userCardState) {
-						userCardState.flipped = true;
-					} else {
-						cards.push({ id, flipped });
-					}
-				} else if (userCardState) {
-					userCardState.flipped = false;
+			if (flipped) {
+				if (userCardState) {
+					userCardState.flipped = true;
+				} else {
+					cards.push({ id, flipped });
 				}
+			} else if (userCardState) {
+				userCardState.flipped = false;
+			}
 
-				patchState(state, { gh: { users: { cards } } });
-			},
-		};
-	}),
+			patchState(state, { gh: { users: { cards } } });
+		}
+	})),
 	withHooks({
 		onInit(state) {
 			console.log('initializing store:', state.gh.users());
