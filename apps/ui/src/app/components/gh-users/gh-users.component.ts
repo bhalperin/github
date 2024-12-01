7b00f09af3c3b84230868b06b4bc86b56cbd935b
Benny Halperin <bhalperin@gmail.com>
2024-12-01 17:36:02 +0200

    Implement previous/next paging in users component
    And utilize tooltip trigger directive

--- apps/ui/src/app/components/gh-users/gh-users.component.ts
+++ apps/ui/src/app/components/gh-users/gh-users.component.ts
@@ -1,17 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit, computed, inject, signal } from '@angular/core';
+import { Component, OnInit, computed, inject, resource, signal } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { GhUser } from '@gh/shared';
 import { first, last } from 'lodash-es';
-import { Observable, firstValueFrom } from 'rxjs';
+import { firstValueFrom } from 'rxjs';
 import { GhUserService } from 'services/gh-user.service';
 import { GhService } from 'services/gh.service';
 import { LoaderDirective } from '../../directives/loader/loader.directive';
+import { TooltipTriggerDirective } from '../../directives/tooltip-trigger/tooltip-trigger.directive';
 import { GhUserComponent } from '../gh-user/gh-user.component';
 
 @Component({
 	selector: 'gh-users',
-	imports: [CommonModule, GhUserComponent, LoaderDirective],
+	imports: [CommonModule, GhUserComponent, LoaderDirective, TooltipTriggerDirective],
 	templateUrl: './gh-users.component.html',
 	styleUrl: './gh-users.component.scss',
 })
@@ -20,21 +21,33 @@ export class GhUsersComponent implements OnInit {
 	readonly #ghService = inject(GhService);
 	readonly #userService = inject(GhUserService);
 	readonly users = signal<GhUser[]>([]);
-	firstUserId = computed(() => first(this.users())?.id);
-	lastUserId = computed(() => last(this.users())?.id);
-	isLoading = signal(true);
-	users$ = new Observable<GhUser[]>();
+	readonly pseudoPageIndex = signal(0);
+	readonly sinceIdList = [] as number[];
+	readonly firstUserId = computed(() => first(this.users())?.id);
+	readonly lastUserId = computed(() => last(this.users())?.id);
+	readonly isLoading = signal(true);
+	readonly incrementPage = () => this.pseudoPageIndex.update((page) => page + 1);
+	readonly decrementPage = () => this.pseudoPageIndex.update((page) => page - 1);
+	readonly usersResource = resource({
+		request: this.pseudoPageIndex,
+		loader: async (params) => {
+			if (params.request < this.sinceIdList.length - 1) {
+				this.sinceIdList.pop();
+			} else {
+				this.sinceIdList.push(this.lastUserId() ?? 0);
+			}
+			await this.getUsers(last(this.sinceIdList));
+		}
+	});
 
 	ngOnInit() {
 		this.#titleService.setTitle('Github users | nest + angular');
 		this.#userService.updateCardFaces(false);
-		this.getNextUsers();
 	}
 
-	protected async getNextUsers() {
+	protected async getUsers(since = 0) {
 		this.isLoading.set(true);
-		this.users$ = this.#ghService.getUsers(this.lastUserId());
-		await firstValueFrom(this.users$)
+		await firstValueFrom(this.#ghService.getUsers(since))
 			.then((users) => this.users.set(users));
 		this.isLoading.set(false);
 	}
