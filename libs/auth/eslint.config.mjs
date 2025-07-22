5b5329151f4f6a130c62545e4a2ef96eb7c241db
Benny Halperin <bhalperin@gmail.com>
2025-07-22 17:05:09 +0300

    Updated eslint config files

---
+++ libs/auth/eslint.config.mjs
@@ -0,0 +1,39 @@
+import baseConfig from '../../eslint.config.mjs';
+
+export default [
+	...baseConfig,
+	{
+		ignores: ['!**/*', '**/generated/*'],
+	},
+	{
+		files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+		rules: {},
+	},
+	{
+		files: ['**/*.ts', '**/*.tsx'],
+		rules: {
+			'@nx/enforce-module-boundaries': [
+				'error',
+				{
+					enforceBuildableLibDependency: true,
+					allow: [],
+
+					depConstraints: [
+						{
+							sourceTag: 'scope:lib-auth',
+							onlyDependOnLibsWithTags: [
+								'scope:lib-config',
+								'scope:lib-prisma',
+								'scope:lib-shared',
+							]
+						},
+					],
+				},
+			],
+		},
+	},
+	{
+		files: ['**/*.js', '**/*.jsx'],
+		rules: {},
+	},
+];
