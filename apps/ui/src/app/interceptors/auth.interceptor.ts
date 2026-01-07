3b1c6e57bdbfdb39e77cceccf807c531d9064ae8
Benny Halperin <bhalperin@gmail.com>
2026-01-07 14:16:10 +0200

    Improved handling error scenarios of token expiry / token refresh time expiry

--- apps/ui/src/app/interceptors/auth.interceptor.ts
+++ apps/ui/src/app/interceptors/auth.interceptor.ts
@@ -1,7 +1,7 @@
 import { HttpErrorResponse, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { AppRouter } from 'fw-extensions/app-router';
-import { catchError, switchMap, tap, throwError } from 'rxjs';
+import { catchError, from, switchMap, tap, throwError } from 'rxjs';
 import { AuthService } from 'services/auth.service';
 import { IS_PUBLIC_API, IS_REFRESH_API } from 'utils/api';
 
@@ -19,14 +19,13 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
 			console.error('*** authInterceptor error = ', error);
 			if (error.status === 401) {
 				if (req.context.get(IS_REFRESH_API)) {
-					router.navigateToTokenExpired();
-
-					return throwError(() => new Error(error.message));
+					return from(router.navigateToTokenExpired()).pipe(
+						switchMap(() => throwError(() => new Error(error.message))),
+					);
 				}
 				return authService.refresh(authService.refreshToken).pipe(
 					tap(() => authService.saveCredentials()),
 					switchMap(() => {
-						authService.saveCredentials();
 						authReq = addToken(req, authService.accessToken);
 
 						return next(authReq);
