14b3c6b852be5937a3a69345bd24b215075d099c
Benny Halperin <bhalperin@gmail.com>
2026-07-15 12:07:41 +0300

    Added error interceptor to capture all API calls errors

--- apps/ui/src/app/app.config.ts
+++ apps/ui/src/app/app.config.ts
@@ -2,17 +2,18 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { provideRouter, Router, withComponentInputBinding } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { errorInterceptor } from './core/error/error-interceptor';
 import { AppRouter } from './fw-extensions/app-router';
 import { authInterceptor } from './interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
 		provideRouter(appRoutes, withComponentInputBinding()),
-		provideHttpClient(withInterceptors([authInterceptor])),
+		provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
 		provideZonelessChangeDetection(),
 		{
 			provide: Router,
-			useClass: AppRouter
+			useClass: AppRouter,
 		},
 	],
 };
