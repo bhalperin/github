bd4cf657c7a9153d15c9abb73b324bc22dd42cf7
Benny Halperin <bhalperin@gmail.com>
2026-08-18 22:05:00 +0300

    Added a console message when starting a unit test

--- apps/ui/src/test-setup.ts
+++ apps/ui/src/test-setup.ts
@@ -1,5 +1,9 @@
-import './styles.scss';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserTestingModule, platformBrowserTesting } from '@angular/platform-browser/testing';
+import chalk from 'chalk';
+
+import './styles.scss';
+
+console.log(chalk.bgBlue.white('🧪 test-setup.ts: Setting up Angular testing environment for Vitest'));
 
 getTestBed().initTestEnvironment(BrowserTestingModule, platformBrowserTesting());
