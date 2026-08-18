02898455424fa15ecc177294f539a22a2e4a450c
Benny Halperin <bhalperin@gmail.com>
2026-08-18 22:10:37 +0300

    Added core url constants to be used instead of local redeclarations

--- apps/ui/src/app/core/utils/api.ts
+++ apps/ui/src/app/core/utils/api.ts
@@ -1,18 +1,21 @@
 import { HttpClient, HttpContext, HttpContextToken } from '@angular/common/http';
 
+export const BASE_API_URL = '/api';
+export const BASE_AUTH_API_URL = `${BASE_API_URL}/auth`;
+export const BASE_GH_API_URL = `${BASE_API_URL}/github`;
 export const IS_PUBLIC_API = new HttpContextToken(() => false);
 export const PUBLIC_API = new HttpContext().set(IS_PUBLIC_API, true);
 export const IS_REFRESH_API = new HttpContextToken(() => false);
 export const REFRESH_API = new HttpContext().set(IS_REFRESH_API, true);
 
 export function publicGet<T>(http: HttpClient, url: string) {
-	return http.get<T>(url, { context: PUBLIC_API});
+	return http.get<T>(url, { context: PUBLIC_API });
 }
 
 export function publicPost<T>(http: HttpClient, url: string, body: T) {
-	return http.post(url, body, { context: PUBLIC_API});
+	return http.post(url, body, { context: PUBLIC_API });
 }
 
 export function refreshPost<T>(http: HttpClient, url: string, body: T) {
-	return http.post(url, body, { context: REFRESH_API});
+	return http.post(url, body, { context: REFRESH_API });
 }
